02929e930b3c84894181319534a31888ee81d15b
Jake Merlin <JacobMerlin92@outlook.com>
2022-06-09 12:51:51 -0400

    using random / string / if statements
    exercise to test knowlegde of creating a simple coin flip.

---
+++ Notes for list.py
@@ -0,0 +1,21 @@
+#Coding BootCamp Challenge Udemy
+#Section 4
+#Notes Lists & appending
+#Jacob A. Merlin
+########################################################
+import random
+########################################################
+#Varible and built list
+fruits = ["apple", "banana"]
+
+#Change item in the list without hard changing the list
+fruits[0] = "mango"
+
+#append function to add items to the end of a list
+fruits.append("kiwi")
+
+#extend function to add a list into a existing list
+fruits.extend(["Menthol", "marlboros"])
+
+#print statement
+print(fruits)
