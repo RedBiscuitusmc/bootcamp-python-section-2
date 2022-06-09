758c2b9973c54fa34542cd304b243db4d1c02da5
Jake Merlin <JacobMerlin92@outlook.com>
2022-06-09 10:49:39 -0400

    Notes and project code used for understanding random and how to use it along with module import.

---
+++ Random Notes Scripts.py
@@ -0,0 +1,18 @@
+#Coding BootCamp Challenge Udemy
+#Section 4
+#Random Module Notes
+#Jacob A. Merlin
+########################################################
+import random
+
+########################################################
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+#Float random
+random_float = random.random() * 5
+print(random_float)
+
+love_Score = random.randint(1, 100)
+print(f"Your love score is {love_Score}.")
