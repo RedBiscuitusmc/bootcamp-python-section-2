1b5a322f2fdabbdd37de3e9f23e556e2a151e679
Jake Merlin <JacobMerlin92@outlook.com>
2022-06-09 10:56:39 -0400

    using random / string / if statements
    exercise to test knowlegde of creating a simple coin flip.

---
+++ Heads or Tails Exercise.py
@@ -0,0 +1,16 @@
+#Coding BootCamp Challenge Udemy
+#Section 4
+#Heads or Tails Exercise
+#Jacob A. Merlin
+########################################################
+import random
+########################################################
+#Varible and control value set
+flip_Flop = random.randint(0, 1)
+########################################################
+#if statement to control for random input.
+if flip_Flop >= 1:
+    print("Heads")
+elif flip_Flop <= 0:
+    print("Tails")
+########################################################
