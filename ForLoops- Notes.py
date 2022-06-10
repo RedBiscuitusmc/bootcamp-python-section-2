9938ca1b08da1483fe73fdca4f3fa178b617c2a4
Jake Merlin <JacobMerlin92@outlook.com>
2022-06-10 08:07:54 -0400

    For loop Notes

---
+++ ForLoops- Notes.py
@@ -0,0 +1,15 @@
+########################################################
+#Coding BootCamp Challenge Udemy
+#Section 5
+#Notes on For-Loops && lists
+#Jacob A. Merlin
+########################################################
+#This is our varible 'fruits'
+#we store data or in this case fruits in a list example 'Apple'
+fruits = [ "Apple", "Peach", "Pear" ]
+#this is our for loop. for loops define varibles in lists items for us automaticlly
+for fruit in fruits:
+    #This set of code runs over and over until the loop of instructions is completed.
+    print(fruit)
+    print(fruit + "pie")
+    print(fruits)
