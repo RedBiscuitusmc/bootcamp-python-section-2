5973b658c0fbf15627e0b6612fcc2a4144d40cb2
Jake Merlin <JacobMerlin92@outlook.com>
2022-06-10 22:24:27 -0400

    notes for using forloop and in-range functions

---
+++ foorloop_in_range.py
@@ -0,0 +1,14 @@
+########################################################
+#Coding BootCamp Challenge Udemy
+#Section 5
+#Notes for loop && range function
+#Jacob A. Merlin
+########################################################
+#########################################################
+#You can add a stepper that increases the count amount by default its one.
+#varible
+total = 0
+#for loop using in range
+for number in range (0, 101):
+    total += number
+print(total)
