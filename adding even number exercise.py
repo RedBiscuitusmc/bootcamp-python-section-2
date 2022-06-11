c0bd0c242595b6e57d561e650308f46d4ca5a516
Jake Merlin <JacobMerlin92@outlook.com>
2022-06-10 22:39:43 -0400

    adding even numbers using the step section of the range function

---
+++ adding even number exercise.py
@@ -0,0 +1,15 @@
+########################################################
+#Coding BootCamp Challenge Udemy
+#Section 5
+#Adding even numbers exercise
+#Jacob A. Merlin
+########################################################
+#########################################################
+#You can add a stepper that increases the count amount by default its one.
+#varible
+total = 0
+#for loop using in range
+for number in range (2, 101, 2):
+    total += number
+
+print(total)
