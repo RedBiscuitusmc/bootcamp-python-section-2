739d158ca0e12a0d526373f0d2ae2be3c234b318
Jake Merlin <JacobMerlin92@outlook.com>
2022-06-13 15:58:10 -0400

    Understanding building functions notes

--- Function Notes.py
+++ Function Notes.py
@@ -9,3 +9,11 @@
 def my_function ():
     print("Hello World")
     print("Goodbye World")
+
+# so to define functions we would
+def my_function():
+#Do this
+#Then Do this
+#Finally do this
+
+#functions do not excute at run time, they must be called from the list if you want to run the logic defined inside of the function.
