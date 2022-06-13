03a8cb6f9452f0f721b1c56ad5ef1b307b7d4303
Jake Merlin <JacobMerlin92@outlook.com>
2022-06-13 15:56:38 -0400

    Understanding building functions notes

---
+++ Function Notes.py
@@ -0,0 +1,11 @@
+########################################################
+#Coding BootCamp Challenge Udemy
+#Section 5
+#Function Notes
+#Jacob A. Merlin
+########################################################
+#########################################################
+#basic function
+def my_function ():
+    print("Hello World")
+    print("Goodbye World")
